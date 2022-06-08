8ebee641d0322dfe3bc9962a05ff2babc1e558e7
brightversion3000 <wuxiaokun@brightvision.onaliyun.com>
2022-06-08 16:38:02 +0800

    修改bug

--- main.py
+++ main.py
@@ -38,12 +38,12 @@ def run(device_id, first_status=False):
         ugc_obj.auto_run(light_screen_stats=False)
         dragon_read.auto_run(light_screen_stats=False)
     else:
-        kuai_shou.auto_run(light_screen_stats=False, watch_ad=True, watch_coin_box=True)
         ugc_lite_obj.auto_run(light_screen_stats=False, watch_video=True, watch_baokuan=False, watch_coin_box=True,
                               watch_ad=True, shopping=True)
         article_lite_opt.auto_run(first_status=first_status, light_screen_stats=False, read_article=True,
                                   watch_small_video=True,
                                   watch_coin_box=True, watch_ad=True, watch_goods=True)
+        kuai_shou.auto_run(light_screen_stats=False, watch_ad=True, watch_coin_box=True)
         ugc_obj.auto_run(light_screen_stats=False, watch_video=False)
         dragon_read.auto_run(light_screen_stats=False)
     total_end_num += 1
